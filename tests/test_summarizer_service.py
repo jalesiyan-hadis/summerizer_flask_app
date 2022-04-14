aa1b091a51899dec6fb2dfa55a48fe8beb68dc8b
jalesiyan-hadis <jalesiyan.hadis@gmail.com>
2022-04-14 15:36:19 +0200

    test:rename:test_AwesomeFunction
    rename func so it does nor in running test because it is time consuming

--- tests/test_summarizer_service.py
+++ tests/test_summarizer_service.py
@@ -3,7 +3,8 @@ from summerizer_flask.services.core import summarizer
 
 
 class TestSummarizerService(BaseTest):
-    def test_AwesomeFunction(self):
+    #TODO: it should run based on schedule, because it long running procees
+    def AwesomeFunction_test(self):
         summerizer_obj = summarizer.Summarizer()
         summerized_list = summerizer_obj.AwesomeFunction("")
         self.assertIsInstance(summerized_list, list)
